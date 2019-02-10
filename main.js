677bac098346f126043fc370e157f4faa1ec44be
lionelMaquet <lionel.maquet@gmail.com>
2019-02-10 23:49:50 +0100

    created knife class

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ let canvasWidth = 1500;
 let canvasHeight = 1000;
 let backgroundColor = 125
 
+let arrayOfObstacles = [];
+
+
+class knife {
+
+  constructor() {
+
+    this.pos = [200, canvasHeight/2]
+    this.width = 20;
+
+  }
+
+  display() {
+    stroke(255)
+    fill(255)
+    ellipse(this.pos[0], this.pos[1], this.width)
+  }
+
+}
+
+
+
+let enemy = new knife();
 
 
 function setup() {
@@ -10,8 +33,16 @@ function setup() {
   background(backgroundColor);
   noFill();
   stroke(255);
+
 }
 
 function draw() {
+  if (arrayOfObstacles.length == 0) {
+    arrayOfObstacles[0] = new knife()
+  }
+
+  arrayOfObstacles[0].display();
+
+  //background(backgroundColor)
 
 }
